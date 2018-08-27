7cc201146121492bfc5cbae3f12bebe73407ba38
Pierre <coaxial@users.noreply.github.com>
2018-08-27 19:46:09 +0000

    Implement role (#1)
    * Update distros
    
    * Add iptables rule
    
    * Implement every iptables module option
    
    * Fix fact gathering timeout
    
    * Set gather_timeout globally
    
    * Decrease uid/gid map offset
    
    * Restore {u,g}id map

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,9 +6,16 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
     os.environ['MOLECULE_INVENTORY_FILE']).get_hosts('all')
 
 
-def test_example(host):
-    file = host.file('/etc/hosts')
+def test_mandatory_vars(host):
+    i = host.iptables
 
-    assert file.exists
-    assert file.user == 'root'
-    assert file.group == 'root'
+    assert '-A INPUT -p tcp -m tcp --dport 1337 -j DROP' in i.rules(
+        'filter', 'INPUT')
+
+
+def test_persist(host):
+    v4 = host.file('/etc/iptables/rules.v4')
+    v6 = host.file('/etc/iptables/rules.v6')
+
+    assert v4.contains('-A INPUT -p tcp -m tcp --dport 1337 -j DROP')
+    assert v6.contains('-A INPUT -p tcp -m tcp --dport 1337 -j DROP')
